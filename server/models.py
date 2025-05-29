91594a6a6b31e7e5201609a84a627fb11080938e
Nili Scharf <nilischarf@gmail.com>
2025-05-29 12:26:34 +0300

    user and workout classes

--- server/models.py
+++ server/models.py
@@ -1,7 +1,9 @@
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import MetaData
+from sqlalchemy.orm import validates
 from sqlalchemy_serializer import SerializerMixin
 from sqlalchemy.ext.associationproxy import association_proxy
+from config import db
 
 metadata = MetaData(naming_convention={
     "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
@@ -9,16 +11,37 @@ metadata = MetaData(naming_convention={
 
 db = SQLAlchemy(metadata=metadata)
 
-from config import db
-
 # Models go here!
+class Workout(db.Model, SerializerMixin):
+    __tablename__ = 'workouts'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    # can add more attributes - difficulty = db.Column(db.Integer)
+
+    calories = db.relationship("Calorie", back_populates="workout", cascade="all, delete")
+    users = association_proxy('calories', 'user')
+    
+    serialize_rules = ('-calories',)
+
+    # add validations if need 
+    
+    def __repr__(self):
+        return f'<Workout {self.id}: {self.name}>'
+
+
 class User(db.Model, SerializerMixin):
     __tablename__ = 'users'
 
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String, unique=True)
 
-    # relationship --> articles = db.relationship('Article', backref='user')
+    calories = db.relationship("Calorie", back_populates="user", cascade="all, delete")
+    workouts = association_proxy('calories', 'workout')
+
+    serialize_rules = ('-calories',)
+
+    # add validations if need 
 
     def __repr__(self):
-        return f'User {self.username}, ID {self.id}'
+        return f'User {self.id}: {self.username}'
