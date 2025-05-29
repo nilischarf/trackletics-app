50afdfdc35e4e457fd3d3fb11ae684aff21466bc
Nili Scharf <nilischarf@gmail.com>
2025-05-29 11:29:45 +0300

    user models.py

---
+++ server/migrations/versions/75e1eabad199_create_db.py
@@ -0,0 +1,24 @@
+"""Create DB
+
+Revision ID: 75e1eabad199
+Revises: 
+Create Date: 2025-05-28 17:59:09.880028
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '75e1eabad199'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
--- server/models.py
+++ server/models.py
@@ -1,6 +1,24 @@
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import MetaData
 from sqlalchemy_serializer import SerializerMixin
 from sqlalchemy.ext.associationproxy import association_proxy
+
+metadata = MetaData(naming_convention={
+    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+})
+
+db = SQLAlchemy(metadata=metadata)
 
 from config import db
 
 # Models go here!
+class User(db.Model, SerializerMixin):
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String, unique=True)
+
+    # relationship --> articles = db.relationship('Article', backref='user')
+
+    def __repr__(self):
+        return f'User {self.username}, ID {self.id}'
