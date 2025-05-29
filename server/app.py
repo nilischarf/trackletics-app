340868632fc446d4615f3d6511074b26d3911000
Nili Scharf <nilischarf@gmail.com>
2025-05-29 11:47:42 +0300

    login app.py

--- server/app.py
+++ server/app.py
@@ -3,20 +3,52 @@
 # Standard library imports
 
 # Remote library imports
-from flask import request
-from flask_restful import Resource
+from flask import Flask, make_response, jsonify, request, session
+from flask_migrate import Migrate
+from flask_restful import Api, Resource
 
 # Local imports
 from config import app, db, api
-# Add your model imports
 
+# Add your model imports
+from models import db, User
+
+app = Flask(__name__)
+app.secret_key = b'Y\xf1Xz\x00\xad|eQ\x80t \xca\x1a\x10K'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.json.compact = False
+
+migrate = Migrate(app, db)
+
+db.init_app(app)
+
+api = Api(app)
 
 # Views go here!
 
-@app.route('/')
-def index():
-    return '<h1>Project Server</h1>'
+@app.route('/') 
+def home():
+    return '<h1>Trackletics</h1>'
 
+class Login(Resource):
+    def post(self):
+        data = request.get_json()
+
+        try: 
+            username = data.get('username')
+
+            new_user = User(username=username)
+            db.session.add(new_user)
+            db.session.commit()
+            response_dict = new_user.to_dict()
+            response = make_response(response_dict, 201)
+            return response
+        
+        except (ValueError, TypeError) as e:
+            return {'errors': ['validation errors']}, 400
+
+api.add_resource(Login, '/login')
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
